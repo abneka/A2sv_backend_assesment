a4a1fd842b18cb9228faf21be13fff8a6284b997
00011244wiut <umidjonaskarov@yandex.com>
2024-07-24 17:49:17 +0300

    feat(backend): add models

---
+++ src/models/comment.model.js
@@ -0,0 +1,27 @@
+﻿const mongoose = require('mongoose');
+
+// Define the Comment schema
+const commentSchema = mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // Reference to the User model
+      required: true,
+    },
+  },
+  {
+    _id: false, // Prevents creating an additional _id for each comment
+  }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
---
+++ src/models/recipe.model.js
@@ -0,0 +1,53 @@
+﻿const mongoose = require('mongoose');
+const { toJSON } = require('./plugins');
+const Comment = require('./comment.model'); // Import the Comment schema
+
+// Define the Recipe schema
+const recipeSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ingredients: [
+      {
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        quantity: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+      },
+    ],
+    instructions: {
+      type: String,
+      required: true,
+    },
+    preparationTime: {
+      type: Number,
+      required: true,
+    },
+    comments: [Comment.schema], // Embed the Comment schema
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // Reference to the User model
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Add plugin that converts mongoose to json
+recipeSchema.plugin(toJSON);
+
+// Define and export the Recipe model
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
