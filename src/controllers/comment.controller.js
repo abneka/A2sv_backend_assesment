aa5e70967a7dcac17e9b5423d40c55ea7321d3d5
00011244wiut <umidjonaskarov@yandex.com>
2024-07-24 17:50:16 +0300

    feat(backend): add controllers

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,113 @@
+﻿const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const Recipe = require('../models/recipe.model');
+
+// Comment Service Functions
+
+const createComment = async (recipeId, commentBody, user) => {
+  const recipe = await Recipe.findById(recipeId);
+  if (!recipe) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Recipe not found');
+  }
+
+  const comment = {
+    ...commentBody,
+    author: user, // Assuming `author` is passed in the request
+  };
+  recipe.comments.push(comment);
+  await recipe.save();
+  return comment;
+};
+
+const getCommentsByRecipeId = async (recipeId) => {
+  const recipe = await Recipe.findById(recipeId).populate('comments.author');
+  if (!recipe) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Recipe not found');
+  }
+  return recipe.comments;
+};
+
+const getCommentById = async (recipeId, commentId) => {
+  const recipe = await Recipe.findById(recipeId);
+  if (!recipe) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Recipe not found');
+  }
+  const comment = recipe.comments.id(commentId);
+  if (!comment) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Comment not found');
+  }
+  return comment;
+};
+
+const updateCommentById = async (recipeId, commentId, updateBody) => {
+  const recipe = await Recipe.findById(recipeId);
+  if (!recipe) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Recipe not found');
+  }
+  const comment = recipe.comments.id(commentId);
+  if (!comment) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Comment not found');
+  }
+  Object.assign(comment, updateBody);
+  await recipe.save();
+  return comment;
+};
+
+const deleteCommentById = async (recipeId, commentId) => {
+  const recipe = await Recipe.findById(recipeId);
+  if (!recipe) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Recipe not found');
+  }
+  const comment = recipe.comments.id(commentId);
+  if (!comment) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Comment not found');
+  }
+  comment.remove();
+  await recipe.save();
+};
+
+// Comment Controller Functions
+
+const createCommentController = catchAsync(async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const user = req.user; // Assuming `user` is attached to the request context
+  const comment = await createComment(recipeId, req.body, user);
+  res.status(httpStatus.CREATED).send(comment);
+});
+
+const getCommentsController = catchAsync(async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const comments = await getCommentsByRecipeId(recipeId);
+  res.send(comments);
+});
+
+const getCommentController = catchAsync(async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const commentId = req.params.commentId;
+  const comment = await getCommentById(recipeId, commentId);
+  res.send(comment);
+});
+
+const updateCommentController = catchAsync(async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const commentId = req.params.commentId;
+  const comment = await updateCommentById(recipeId, commentId, req.body);
+  res.send(comment);
+});
+
+const deleteCommentController = catchAsync(async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const commentId = req.params.commentId;
+  await deleteCommentById(recipeId, commentId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+module.exports = {
+  createCommentController,
+  getCommentsController,
+  getCommentController,
+  updateCommentController,
+  deleteCommentController,
+};
---
+++ src/controllers/recipe.controller.js
@@ -0,0 +1,87 @@
+﻿const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const Recipe = require('../models/recipe.model');
+
+// Recipe Service Functions
+
+const createRecipe = async (recipeBody) => {
+  console.log(recipeBody);
+  return Recipe.create(recipeBody);
+};
+
+const queryRecipes = async (filter, options) => {
+  return Recipe.find()
+};
+
+const getRecipeById = async (recipeId) => {
+  return Recipe.findById(recipeId);
+};
+
+const updateRecipeById = async (recipeId, updateBody) => {
+  const recipe = await Recipe.findById(recipeId);
+  if (!recipe) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Recipe not found');
+  }
+  Object.assign(recipe, updateBody);
+  await recipe.save();
+  return recipe;
+};
+
+const deleteRecipeById = async (recipeId) => {
+  const recipe = await Recipe.findById(recipeId);
+  if (!recipe) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Recipe not found');
+  }
+  await recipe.remove();
+};
+
+// Recipe Controller Functions
+
+const createRecipeController = catchAsync(async (req, res) => {
+  const { title, ingredients, instructions, preparationTime } = req.body;
+  const user = req.user; // Extracted user from token
+
+  const recipe = await Recipe.create({
+    title,
+    ingredients,
+    instructions,
+    preparationTime,
+    creator: user._id, // Set creator as the logged-in user
+  });
+  res.status(httpStatus.CREATED).send(recipe);
+});
+
+const getRecipesController = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ['title', 'creator']);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+  const result = await queryRecipes(filter, options);
+  res.send(result);
+});
+
+const getRecipeController = catchAsync(async (req, res) => {
+  const recipe = await getRecipeById(req.params.recipeId);
+  if (!recipe) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Recipe not found');
+  }
+  res.send(recipe);
+});
+
+const updateRecipeController = catchAsync(async (req, res) => {
+  const recipe = await updateRecipeById(req.params.recipeId, req.body);
+  res.send(recipe);
+});
+
+const deleteRecipeController = catchAsync(async (req, res) => {
+  await deleteRecipeById(req.params.recipeId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+module.exports = {
+  createRecipeController,
+  getRecipesController,
+  getRecipeController,
+  updateRecipeController,
+  deleteRecipeController,
+};
